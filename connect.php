70ea0fd0b1d2d4658b8216e823646c0b4222b79c
mytaayu <37535575+mytaayu@users.noreply.github.com>
2018-04-23 20:00:11 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$host='sql104.epizy.com';
+$user='epiz_21941247';
+$pass='mitaisti13';
+$database='epiz_21941247_dbstbi';
+
+$conn=mysqli_connect($host,$user,$pass);
+mysqli_select_db($conn, $database);
+
+?>
